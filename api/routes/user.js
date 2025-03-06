c4727e88c2b27774fd4d3694b605c2fe0d7625bb
RamadhanAdam <ramadhanzome@gmail.com>
2025-03-06 16:57:57 +0300

    Added user signup and ability to delete users, hashing passwords and validations

---
+++ api/routes/user.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/product");
+const bcrypt = require('bcrypt')
+const User = require('../models/user');
+const mongoose = require('mongoose')
+
+router.post('/signup', (req, res, next) => {
+
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: "Mail exists"
+                })
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        })
+                    } else {
+                        const user = new User({
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user.save()
+                            .then(result => {
+                                console.log(result)
+                                res.status(200).json({
+                                    message: "User created"
+                                })
+                            })
+                            .catch(err => {
+                                console.log(err)
+                                res.status(500).json({
+                                    error: err
+                                })
+                            })
+                    }
+                })
+
+            }
+
+        })
+
+});
+
+
+router.delete('/:UserId', (req, res, next) => {
+
+    User.deleteOne({ _id: req.params.UserId})
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "User Deleted"
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
+})
+
+module.exports = router;
